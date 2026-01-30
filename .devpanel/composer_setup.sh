fbab346887289095cc47772742ad4ebf2500b6c1
Darren Oh <9472-darrenoh@users.noreply.drupalcode.org>
2026-01-30 08:21:31 -0500

    Remove Composer Patches

--- .devpanel/composer_setup.sh
+++ .devpanel/composer_setup.sh
@@ -8,9 +8,6 @@ composer create-project --no-install ${PROJECT:=drupal/recommended-project}:^11
 cp -r "${PROJECT#*/}"/* ./
 rm -rf "${PROJECT#*/}" patches.lock.json
 
-# Programmatically fix Composer 2.2 allow-plugins to avoid errors.
-composer config --no-plugins allow-plugins.cweagans/composer-patches true
-
 # Scaffold settings.php.
 composer config -jm extra.drupal-scaffold.file-mapping '{
     "[web-root]/sites/default/settings.php": {
@@ -21,7 +18,5 @@ composer config -jm extra.drupal-scaffold.file-mapping '{
 composer config scripts.post-drupal-scaffold-cmd \
     'cd web/sites/default && test -z "$(grep '\''include \$devpanel_settings;'\'' settings.php)" && patch -Np1 -r /dev/null < $APP_ROOT/.devpanel/drupal-settings.patch || :'
 
-# Add Drush and Composer Patches.
-composer require -n --no-update \
-    drush/drush \
-    cweagans/composer-patches
+# Add Drush.
+composer require -n --no-update drush/drush
